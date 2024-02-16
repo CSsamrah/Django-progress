5f9854576e4b9478f89a48498ad8ee13692e1fb2
CSSamrah <samra.fatima.790@gmail.com>
2024-02-16 23:36:34 +0500

    update

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground.apps.PlaygroundConfig',
+    'playground',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
