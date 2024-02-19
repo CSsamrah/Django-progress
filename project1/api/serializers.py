9f5556a60140b55f9080a2e24d5422af5ad7e6a3
CSSamrah <samra.fatima.790@gmail.com>
2024-02-19 22:59:11 +0500

    JSON Renderer

--- project1/api/serializers.py
+++ project1/api/serializers.py
@@ -1,19 +1,11 @@
 from rest_framework import serializers
-
-
+from .models import Student
+# from rest_framework.renderers import JSONRenderer
 
 class StudentSerializer(serializers.Serializer):
     name=serializers.CharField(max_length=100)
     roll=serializers.IntegerField()
     city=serializers.CharField(max_length=100)
-#creating model instance stu
 
-stu=Student.objects.get(id=1)
-#converting model instance stu to python dict/serializing object
-serializer=StudentSerializer(stu)
 
-#creating query set
-stu=Student.ojects.all()
-# converting query set stu to list of python dict/serializing query set
-serializer=StudentSerializer(stu,many=True)
-print (serializer.data)
+
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -3,13 +3,13 @@ import json
 
 # endpoint="https://httpbin.org/status/200"
 # endpoint="https://httpbin.org" # simple http request gives response in HTML form 
-endpoint="http://127.0.0.1:8000/api/hello"  
+endpoint="http://127.0.0.1:8000/api/studinfo/2"  
 # rest API gives response in JSON 
 get_response=requests.get(endpoint)
 print(get_response.text)
 
 
-print(get_response.json()['name'])  
+print(get_response.json())  
 # converted into python dictionary
 # print("\n") 
 # print(get_response.status_code)
@@ -21,6 +21,6 @@ print(get_response.json()['name'])
 # python_data={'name':'Samrah', 'roll_no':'SE-22-01'}
 # json_data=json.dumps(python_data)
 # print(json_data)
-# #loads method(used to parse json string)
+# # #loads method(used to parse json string)
 # parsed_data=json.loads(json_data)
 # print(parsed_data) #converts json into python
